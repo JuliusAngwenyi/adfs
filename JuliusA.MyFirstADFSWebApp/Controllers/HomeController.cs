2dd82afca465857ee990cb9658cfc4babd9fe116
Julius A <julius.bichage@gmail.com>
2018-05-31 11:17:24 +0100

    Added logic to display user claims

--- JuliusA.MyFirstADFSWebApp/Controllers/HomeController.cs
+++ JuliusA.MyFirstADFSWebApp/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using JuliusA.MyFirstADFSWebApp.Helpers;
+using JuliusA.MyFirstADFSWebApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -26,6 +29,19 @@ namespace JuliusA.MyFirstADFSWebApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult UserClaims()
+        {
+            var principal = IdentityHelper.GetCurrentClaimsPrincipal();
+
+            // temporary code to show user claims, while there is a sitecore user object as
+            UserClaimsModel ucm = new UserClaimsModel();
+            if (principal != null)
+            {
+                ucm.Claims = ((ClaimsPrincipal)principal).Claims;
+            }
+            ViewBag.Title = "ADFS Claims page";
+            return View(ucm);
         }
     }
 }
---
+++ JuliusA.MyFirstADFSWebApp/Models/UserClaimsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Linq;
+using System.Web;
+
+namespace JuliusA.MyFirstADFSWebApp.Models
+{
+    public class UserClaimsModel
+    {
+        public IEnumerable<Claim> Claims { get; set; }
+    }
+}
